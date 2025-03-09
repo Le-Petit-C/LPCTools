ef8eea3181678dc5a132aa18ee388183e0a5e8d4
Le_Petit_C <2729362136@qq.com>
2025-03-09 14:34:55 +0800

    1.20.4

--- src/client/java/lpctools/tools/fillingassistant/Data.java
+++ src/client/java/lpctools/tools/fillingassistant/Data.java
@@ -20,8 +20,8 @@ public class Data {
     @NotNull public static ImmutableList<Block> defaultRequiredBlockWhiteList = initDefaultRequiredBlockWhiteList();
     @NotNull public static ImmutableList<String> defaultRequiredBlockIdList = idListFromBlockList(defaultRequiredBlockWhiteList);
 
-    public static String getItemId(Item item){return Registries.ITEM.getEntry(item).toString();}
-    public static String getBlockId(Block block){return Registries.BLOCK.getEntry(block).toString();}
+    public static String getItemId(Item item){return Registries.ITEM.getId(item).toString();}
+    public static String getBlockId(Block block){return Registries.BLOCK.getId(block).toString();}
     @NotNull private static ImmutableList<Item> initDefaultPlaceableItemList(){
         return ImmutableList.of(
                 Items.STONE,
