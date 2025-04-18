9643e3aaa02c8ab522d980c6986db216e76fc716
Le_Petit_C <2729362136@qq.com>
2025-04-18 19:25:04 +0800

    完善了SlightXRay的初始化和多线程初始化

--- src/client/java/lpctools/tools/singletool/SlightXRay.java
+++ src/client/java/lpctools/tools/singletool/SlightXRay.java
@@ -17,18 +17,19 @@ import net.minecraft.client.render.BuiltBuffer;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import net.minecraft.util.math.Direction;
+import net.minecraft.world.chunk.WorldChunk;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
-import java.util.HashSet;
+import java.util.*;
 
 import static lpctools.util.MathUtils.*;
 
 public class SlightXRay implements IValueRefreshCallback, WorldRenderEvents.End {
+    //markedBlocks放在多线程里用，记得要同步
     @NotNull final HashSet<BlockPos> markedBlocks = new HashSet<>();
-    @NotNull final HashSet<Block> XRayBlocks = initHashset();
+    @NotNull static final HashSet<Block> XRayBlocks = initHashset();
     private static HashSet<Block> initHashset(){
         HashSet<Block> blocks = new HashSet<>();
         blocks.add(Blocks.DIAMOND_ORE);
@@ -47,14 +48,19 @@ public class SlightXRay implements IValueRefreshCallback, WorldRenderEvents.End 
                 for(int x = chunkPos.x - distance; x <= chunkPos.x + distance; ++x){
                     for(int z = chunkPos.z - distance; z <= chunkPos.z + distance; ++z){
                         if(world.isChunkLoaded(x, z))
-                            loadChunk(world, new ChunkPos(x, z));
+                            newAThreadToLoadChunk(world, new ChunkPos(x, z), player);
                     }
                 }
             }
         }
         else {
             Registry.unregisterWorldRenderEndCallback(this);
-            markedBlocks.clear();
+            synchronized (markedBlocks){
+                markedBlocks.clear();
+            }
+            synchronized (threadTasks){
+                threadTasks.clear();
+            }
         }
     }
 
@@ -62,10 +68,10 @@ public class SlightXRay implements IValueRefreshCallback, WorldRenderEvents.End 
         RenderContext ctx = new RenderContext(MaLiLibPipelines.POSITION_COLOR_MASA_NO_DEPTH);
         BufferBuilder buffer = ctx.getBuilder();
         Matrix4f matrix = worldToCameraMatrix(context.camera());
-        //Matrix4f matrix = inverseOffsetMatrix4f(context.camera().getPos().toVector3f());
-        //Matrix4f matrix = new Matrix4f();
-        for(BlockPos pos : markedBlocks)
-            vertexBlock(matrix, buffer, pos, 0x3F3F7FFF);
+        synchronized (markedBlocks){
+            for(BlockPos pos : markedBlocks)
+                vertexBlock(matrix, buffer, pos, 0x7F3F7FFF);
+        }
         try {
             BuiltBuffer meshData = buffer.endNullable();
             if (meshData != null) {
@@ -78,20 +84,154 @@ public class SlightXRay implements IValueRefreshCallback, WorldRenderEvents.End 
         }
     }
 
+    private enum XRayNecessaryState{
+        F_F(false, false),
+        F_T(false, true),
+        T_F(true, false),
+        T_T(true, true);
+        public final boolean doShowAround, isXRayTarget;
+        public static XRayNecessaryState of(BlockState state){
+            return values()[(XRayBlocks.contains(state.getBlock()) ? 1 : 0)
+                    + (!state.isOpaque() || state.isTransparent() ? 2 : 0)];
+        }
+        XRayNecessaryState(boolean doShowAround, boolean isXRayTarget){
+            this.doShowAround = doShowAround;
+            this.isXRayTarget = isXRayTarget;
+        }
+    }
+
     private void loadChunk(ClientWorld world, ChunkPos chunkPos){
         //TODO:优化算法
-        for(BlockPos pos : BlockPos.iterate(
-                chunkPos.getStartX(), world.getBottomY() - 1, chunkPos.getStartZ(),
-                chunkPos.getEndX(), world.getBottomY() + world.getHeight() + 1, chunkPos.getEndZ()
-        )){
-            BlockState state = world.getBlockState(pos);
-            if(!state.isOpaque() || state.isTransparent()){
-                for(Direction direction : Direction.values()){
-                    BlockPos testPos = pos.offset(direction);
-                    if(XRayBlocks.contains(world.getBlockState(testPos).getBlock()))
-                        markedBlocks.add(testPos);
+        int cx = chunkPos.x, cz = chunkPos.z;
+        if(!world.isChunkLoaded(cx, cz)) return;
+        WorldChunk chunk = world.getChunk(cx, cz);
+        int minY = world.getBottomY();
+        int numY = world.getHeight();
+        int topY = minY + numY;
+
+        //states用于存放预处理后的数据，并向外拓展了两格防止越界
+        XRayNecessaryState[][][] states = new XRayNecessaryState[18][numY + 2][18];
+        //初始化states数据
+        for(XRayNecessaryState[][] states1 : states){
+            for(XRayNecessaryState[] states2 : states1){
+                Arrays.fill(states2, XRayNecessaryState.F_F);
+            }
+        }
+        //加载本区块中数据
+        BlockPos.Mutable mutableBlockPos = new BlockPos.Mutable();
+        for(int x = 0; x < 16; ++x){
+            mutableBlockPos.setX(x);
+            XRayNecessaryState[][] states1 = states[x + 1];
+            for(int y = minY; y < topY; ++y){
+                mutableBlockPos.setY(y);
+                XRayNecessaryState[] states2 = states1[y - minY + 1];
+                for(int z = 0; z < 16; ++z){
+                    mutableBlockPos.setZ(z);
+                    states2[z + 1] = XRayNecessaryState.of(chunk.getBlockState(mutableBlockPos));
                 }
             }
+        }
+        //加载相邻区块中数据
+        WorldChunk nxChunk = world.isChunkLoaded(cx - 1, cz) ? world.getChunk(cx - 1, cz) : null;
+        WorldChunk pxChunk = world.isChunkLoaded(cx + 1, cz) ? world.getChunk(cx + 1, cz) : null;
+        WorldChunk nzChunk = world.isChunkLoaded(cx, cz - 1) ? world.getChunk(cx, cz - 1) : null;
+        WorldChunk pzChunk = world.isChunkLoaded(cx, cz + 1) ? world.getChunk(cx, cz + 1) : null;
+        if(nxChunk != null){
+            mutableBlockPos.setX(-1);
+            for(int y = minY; y < topY; ++y){
+                mutableBlockPos.setY(y);
+                for(int z = 0; z < 16; ++z){
+                    mutableBlockPos.setZ(z);
+                    states[0][y - minY + 1][z + 1] = XRayNecessaryState.of(nxChunk.getBlockState(mutableBlockPos));
+                }
+            }
+        }
+        if(pxChunk != null){
+            mutableBlockPos.setX(16);
+            for(int y = minY; y < topY; ++y){
+                mutableBlockPos.setY(y);
+                for(int z = 0; z < 16; ++z){
+                    mutableBlockPos.setZ(z);
+                    states[17][y - minY + 1][z + 1] = XRayNecessaryState.of(pxChunk.getBlockState(mutableBlockPos));
+                }
+            }
+        }
+        if(nzChunk != null){
+            mutableBlockPos.setZ(-1);
+            for(int y = minY; y < topY; ++y){
+                mutableBlockPos.setY(y);
+                for(int x = 0; x < 16; ++x){
+                    mutableBlockPos.setX(x);
+                    states[x + 1][y - minY + 1][0] = XRayNecessaryState.of(nzChunk.getBlockState(mutableBlockPos));
+                }
+            }
+        }
+        if(pzChunk != null){
+            mutableBlockPos.setZ(16);
+            for(int y = minY; y < topY; ++y){
+                mutableBlockPos.setY(y);
+                for(int x = 0; x < 16; ++x){
+                    mutableBlockPos.setX(x);
+                    states[x + 1][y - minY + 1][17] = XRayNecessaryState.of(pzChunk.getBlockState(mutableBlockPos));
+                }
+            }
+        }
+        //检测并加入过关数据
+        for(int x = 1; x <= 16; ++x){
+            for(int y = 1; y <= numY; ++y){
+                for(int z = 1; z <= 16; ++z){
+                    if(!states[x][y][z].doShowAround) continue;
+                    if(states[x - 1][y][z].isXRayTarget) markPos(x - 2, y + minY - 1, z - 1, chunkPos);
+                    if(states[x + 1][y][z].isXRayTarget) markPos(x, y + minY - 1, z - 1, chunkPos);
+                    if(states[x][y - 1][z].isXRayTarget) markPos(x - 1, y + minY - 2, z - 1, chunkPos);
+                    if(states[x][y + 1][z].isXRayTarget) markPos(x - 1, y + minY, z - 1, chunkPos);
+                    if(states[x][y][z - 1].isXRayTarget) markPos(x - 1, y + minY - 1, z - 2, chunkPos);
+                    if(states[x][y][z + 1].isXRayTarget) markPos(x - 1, y + minY - 1, z, chunkPos);
+                }
+            }
+        }
+    }
+
+    private static final PriorityQueue<ThreadTask> threadTasks = new PriorityQueue<>(new ThreadTask.Comparator());
+    private static Thread thread;
+    private record ThreadTask(double distance, ClientWorld world, ChunkPos chunkPos){
+        public static class Comparator implements java.util.Comparator<ThreadTask>{
+            @Override public int compare(ThreadTask o1, ThreadTask o2) {
+                return (int)(o1.distance - o2.distance);
+            }
+        }
+        ThreadTask(ClientWorld world, ChunkPos pos, ClientPlayerEntity player){
+            this(pos.getCenterAtY((int)player.getPos().getY()).getSquaredDistance(player.getPos()),
+                    world, pos);
+        }
+    }
+
+    private void newAThreadToLoadChunk(ClientWorld world, ChunkPos chunkPos, ClientPlayerEntity player){
+        synchronized (threadTasks){
+            threadTasks.add(new ThreadTask(world, chunkPos, player));
+            if(thread == null){
+                thread = new Thread(this::ThreadFunc);
+                thread.start();
+            }
+        }
+    }
+    private void ThreadFunc(){
+        while(true){
+            ThreadTask task;
+            synchronized (threadTasks){
+                if(threadTasks.isEmpty()){
+                    thread = null;
+                    break;
+                }
+                task = threadTasks.remove();
+            }
+            loadChunk(task.world, task.chunkPos);
+        }
+    }
+
+    private void markPos(int x, int y, int z, ChunkPos chunkPos){
+        synchronized (markedBlocks){
+            markedBlocks.add(chunkPos.getStartPos().add(x, y, z));
         }
     }
 
