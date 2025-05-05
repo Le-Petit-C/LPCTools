d6537f5eb384f9762468bf07e340ac59fc31aea9
Le_Petit_C <2729362136@qq.com>
2025-05-05 10:39:38 +0800

    将流体清理工具的遍历顺序改为了由近到远

--- src/client/java/lpctools/util/AlgorithmUtils.java
+++ src/client/java/lpctools/util/AlgorithmUtils.java
@@ -1,12 +1,17 @@
 package lpctools.util;
 
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.PriorityQueue;
 
 import static lpctools.util.MathUtils.*;
 
+@SuppressWarnings("unused")
 public class AlgorithmUtils {
     public static Iterable<BlockPos> iterateInManhattanDistance(BlockPos center, int distance){
         return new Iterable<>(){
@@ -39,5 +44,61 @@ public class AlgorithmUtils {
                 };
             }
         };
+    }
+    //从近到远遍历方块
+    public static class NearstIterator implements Iterator<BlockPos>{
+        private final @NotNull Vec3d center;
+        protected final PriorityQueue<BlockPos> poses;
+        protected final HashSet<BlockPos> posSet;
+        NearstIterator(@NotNull Vec3d center){
+            this.center = center;
+            poses = new PriorityQueue<>(
+                    (o1, o2) -> {
+                        double v = getDistance(o1) - getDistance(o2);
+                        return v == 0 ? 0 : (v < 0 ? -1 : 1);
+                    }
+            );
+            BlockPos pos = BlockPos.ofFloored(center);
+            for (int x = 0; x < 2; ++x)
+                for (int y = 0; y < 2; ++y)
+                    for (int z = 0; z < 2; ++z)
+                        poses.add(pos.add(x, y, z));
+            posSet = new HashSet<>(poses);
+        }
+        public double getDistance(BlockPos pos) {
+            return pos.getSquaredDistance(center);
+        }
+        public double getNextDistance(){
+            assert !poses.isEmpty();
+            return getDistance(poses.peek());
+        }
+        @Override public boolean hasNext() {return true;}
+        @Override public BlockPos next() {
+            assert !poses.isEmpty();
+            BlockPos ret = poses.remove();
+            posSet.remove(ret);
+            for (Direction direction : Direction.values()) {
+                BlockPos nextPos = ret.offset(direction);
+                if (getDistance(nextPos) <= getDistance(ret)) continue;
+                if (posSet.contains(nextPos)) continue;
+                posSet.add(nextPos);
+                poses.add(nextPos);
+            }
+            return ret;
+        }
+    }
+    public static Iterable<BlockPos> iterateNears(Vec3d center){
+        return () -> new NearstIterator(center);
+    }
+    public static class InNearstIterator extends NearstIterator{
+        public final double maxSquaredDistance;
+        InNearstIterator(@NotNull Vec3d center, double maxDistance) {
+            super(center);
+            maxSquaredDistance = maxDistance * maxDistance;
+        }
+        @Override public boolean hasNext() {return getNextDistance() <= maxSquaredDistance;}
+    }
+    public static Iterable<BlockPos> iterateInNears(Vec3d center, double distance){
+        return () -> new InNearstIterator(center, distance);
     }
 }
