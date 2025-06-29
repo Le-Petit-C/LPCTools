e1605516c3fbe88458b43f159b90e77f2e8888ca
Le_Petit_C <2729362136@qq.com>
2025-06-29 11:22:03 +0800

    1.20.3-1.20.4

--- src/client/java/lpctools/lpcfymasaapi/configbutton/derivedConfigs/ReachDistanceConfig.java
+++ src/client/java/lpctools/lpcfymasaapi/configbutton/derivedConfigs/ReachDistanceConfig.java
@@ -6,8 +6,7 @@ import lpctools.lpcfymasaapi.interfaces.ILPCConfigList;
 import lpctools.lpcfymasaapi.interfaces.ILPCValueChangeCallback;
 import lpctools.util.AlgorithmUtils;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.NotNull;
@@ -25,10 +24,10 @@ public class ReachDistanceConfig extends DoubleConfig {
         return AlgorithmUtils.iterateFromFurthestInDistance(center, getAsDouble());
     }
     @Override public void refreshName(boolean align) {
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        if(GenericConfigs.reachDistanceAlwaysUnlimited.getAsBoolean() || player == null)
+        ClientPlayerInteractionManager itm = MinecraftClient.getInstance().interactionManager;
+        if(GenericConfigs.reachDistanceAlwaysUnlimited.getAsBoolean() || itm == null)
             setMax(Double.MAX_VALUE);
-        else setMax(PlayerEntity.getReachDistance(player.isCreative()));
+        else setMax(itm.getReachDistance());
         super.refreshName(align);
     }
     @Override public @NotNull String getFullTranslationKey() {
