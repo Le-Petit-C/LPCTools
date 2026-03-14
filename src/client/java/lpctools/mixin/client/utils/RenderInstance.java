d0f20449fea1021269fc86baeb3a1fbc869c0b0f
Le_Petit_C <2729362136@qq.com>
2026-03-14 12:06:47 +0800

    熔炉组维护工具功能修正，同时修复了一个生产环境的Mixin问题

--- src/client/java/lpctools/mixin/client/utils/RenderInstance.java
+++ src/client/java/lpctools/mixin/client/utils/RenderInstance.java
@@ -6,11 +6,15 @@ import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.util.math.MatrixStack;
 import org.joml.Matrix4f;
-import org.joml.Vector3f;
+import org.joml.Quaternionf;
+import org.joml.Vector3fc;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import static lpctools.lpcfymasaapi.render.translucentShapes.RenderInstance.*;
 
@@ -29,10 +33,21 @@ public class RenderInstance {
 	void recordMatrixStack(RenderTickCounter renderTickCounter, CallbackInfo ci, @Local MatrixStack matrixStack) {
 		worldProjectionTranslateMatrix.set(matrixStack.peek().getPositionMatrix());
 	}
-	@Inject(method = "renderWorld", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;rotate(FLorg/joml/Vector3fc;)Lorg/joml/Matrix4f;", ordinal = 0))
-	void recordRotateEffects(RenderTickCounter renderTickCounter, CallbackInfo ci, @Local(name = "m") float m, @Local Vector3f vector3f, @Local(name = "l") float l) {
-		Matrix4f A = new Matrix4f().rotate(m, vector3f).scale(1.0F / l, 1.0F, 1.0F).rotate(-m, vector3f);
-		worldBasicProjectionMatrix.mul(A);
-		A.invert(new Matrix4f()).mul(worldProjectionTranslateMatrix, worldProjectionTranslateMatrix).mul(A);
+	@ModifyArgs(method = "renderWorld", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;scale(FFF)Lorg/joml/Matrix4f;", remap = false))
+	void recordScaleEffects(Args args) {
+		float x = args.get(0);
+		float y = args.get(1);
+		float z = args.get(2);
+		worldBasicProjectionMatrix.scale(x, y, z);
+		worldProjectionTranslateMatrix.scaleLocal(1.0f / x, 1.0f / y, 1.0f / z).scale(x, y, z);
+	}
+	@Unique Quaternionf qCache = new Quaternionf();
+	@ModifyArgs(method = "renderWorld", at = @At(value = "INVOKE", target = "Lorg/joml/Matrix4f;rotate(FLorg/joml/Vector3fc;)Lorg/joml/Matrix4f;", remap = false))
+	void recordRotateEffects(Args args) {
+		float angle = args.get(0);
+		Vector3fc axis = args.get(1);
+		qCache.fromAxisAngleRad(axis, angle);
+		worldBasicProjectionMatrix.rotate(qCache);
+		worldProjectionTranslateMatrix.rotate(qCache).rotateLocal(qCache.conjugate());
 	}
 }
