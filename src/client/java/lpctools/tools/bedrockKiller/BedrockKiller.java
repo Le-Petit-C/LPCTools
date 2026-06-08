100c3c6447b55384d79b58fa352f0796c8d1dfe5
Le_Petit_C <2729362136@qq.com>
2026-06-08 13:35:14 +0800

    添加了基岩破坏工具“多重采样”的功能

--- src/client/java/lpctools/tools/bedrockKiller/BedrockKiller.java
+++ src/client/java/lpctools/tools/bedrockKiller/BedrockKiller.java
@@ -2,6 +2,7 @@ package lpctools.tools.bedrockKiller;
 
 import lpctools.lpcfymasaapi.Registries;
 import lpctools.lpcfymasaapi.configButtons.transferredConfigs.BooleanConfig;
+import lpctools.lpcfymasaapi.configButtons.transferredConfigs.IntegerConfig;
 import lpctools.lpcfymasaapi.configButtons.uniqueConfigs.BooleanHotkeyThirdListConfig;
 import lpctools.lpcfymasaapi.interfaces.ILPCValueChangeCallback;
 import lpctools.mixin.client.accessors.MinecraftAccessor;
@@ -23,42 +24,69 @@ public class BedrockKiller {
 	public static final BooleanHotkeyThirdListConfig BKConfig = new BooleanHotkeyThirdListConfig(ToolConfigs.toolConfigs, "BK", BedrockKillerEvents.INSTANCE);
 	static { ToolUtils.setLPCToolsToggleText(BKConfig); }
 	static { listStack.push(BKConfig); }
-	public static final BooleanConfig autoOperateWhenLookingRedstoneTorch = addBooleanConfig("autoOperateWhenLookingRedstoneTorch", false, BedrockKillerEvents.INSTANCE);
+	public static final BooleanConfig torchAutoOperate = addBooleanConfig("torchAutoOperate", false, BedrockKillerEvents.INSTANCE);
+	public static final IntegerConfig multiSamplingFactor = addIntegerConfig("multiSamplingFactor", 1, 1, 64);
 	static { listStack.pop(); }
 	
 	private static boolean operating = false;
 	
 	public static void operate() {
-		if(!BKConfig.getBooleanValue() || operating) return;
-		operating = true;
-		try{
-			Minecraft mc = Minecraft.getInstance();
-			LocalPlayer player = mc.player;
-			Entity camera = mc.getCameraEntity();
-			if(player == null || camera == null) return;
-			MinecraftAccessor accessor = (MinecraftAccessor)mc;
-			mc.hitResult = player.raycastHitResult(0, camera);
-			accessor.invokeStartAttack();
-			accessor.invokeStartUseItem();
-		} finally {
-			operating = false;
-		}
+		BedrockKillerEvents.INSTANCE.operate();
 	}
 	
 	private static class BedrockKillerEvents implements ClientTickEvents.EndTick, ILPCValueChangeCallback {
 		static final BedrockKillerEvents INSTANCE = new BedrockKillerEvents();
-		@Override public void onEndTick(@NonNull Minecraft mc) {
-			ClientLevel level = mc.level;
+
+		float partialTicks = 1.0f;
+
+		void operate() {
+			if(!BKConfig.getBooleanValue() || operating) return;
+			operating = true;
+			try{
+				Minecraft mc = Minecraft.getInstance();
+				LocalPlayer player = mc.player;
+				Entity camera = mc.getCameraEntity();
+				if(player == null || camera == null) return;
+				MinecraftAccessor accessor = (MinecraftAccessor)mc;
+				mc.hitResult = player.raycastHitResult(partialTicks, camera);
+				accessor.invokeStartAttack();
+				accessor.invokeStartUseItem();
+			} finally {
+				operating = false;
+			}
+		}
+
+		boolean tryOperate(Minecraft mc, ClientLevel level) {
 			HitResult hitResult = mc.hitResult;
 			if(level != null && hitResult instanceof BlockHitResult blockHitResult
-					&& level.getBlockState(blockHitResult.getBlockPos()).getBlock() == Blocks.REDSTONE_TORCH) {
+				&& level.getBlockState(blockHitResult.getBlockPos()).getBlock() == Blocks.REDSTONE_TORCH) {
 				MinecraftAccessor accessor = (MinecraftAccessor)mc;
 				accessor.invokeStartAttack();
+				return true;
+			}
+			else return false;
+		}
+
+		@Override public void onEndTick(@NonNull Minecraft mc) {
+			LocalPlayer player = mc.player;
+			Entity camera = mc.getCameraEntity();
+			ClientLevel level = mc.level;
+			int multiSamplingCount = multiSamplingFactor.getAsInt();
+			if(player != null && camera != null && level != null) {
+				HitResult originHitResult = mc.hitResult;
+				for(int i = 1; i < multiSamplingCount; ++i) {
+					partialTicks = (float)i /  multiSamplingCount;
+					mc.hitResult = player.raycastHitResult(partialTicks, camera);
+					if(tryOperate(mc, level)) return;
+				}
+				partialTicks = 1.0f;
+				mc.hitResult = originHitResult;
+				tryOperate(mc, level);
 			}
 		}
 
 		@Override public void onValueChanged() {
-			Registries.END_CLIENT_TICK.register(this, BKConfig.getBooleanValue() && autoOperateWhenLookingRedstoneTorch.getBooleanValue());
+			Registries.END_CLIENT_TICK.register(this, BKConfig.getBooleanValue() && torchAutoOperate.getBooleanValue());
 		}
 	}
 }
